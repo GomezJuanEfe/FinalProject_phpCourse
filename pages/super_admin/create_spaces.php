2858e934f15d1fd88456c6cfa714cc7a10726c1b
Juan Felipe Gomez <jf.gomezramirez@gmail.com>
2022-05-14 01:13:59 -0700

    Admin functions Done :)

--- pages/super_admin/create_spaces.php
+++ pages/super_admin/create_spaces.php
@@ -1,4 +1,45 @@
+<h2>Spaces</h2>
+<?php
+$dbcon = new mysqli($DBserver, $username, $password, $dbName);
+if ($dbcon->connect_error) {
+    die("DB error");
+} else {
+    $selectQuery = "SELECT s.space_id,s.type,n.name FROM spaces_tb s INNER JOIN locations_tb n on s.location_id=n.location_id ORDER BY n.name, s.type";
+    $spacesList = $dbcon->query($selectQuery);
+    if ($spacesList->num_rows > 0) {
+        echo "<table><tr><th>ID</th><th>Type</th><th>Location</th></tr>";
+        while ($space = $spacesList->fetch_assoc()) {
+            echo "<tr><td>" . $space['space_id'] . "</td><td>" . $space['type'] . "</td><td>" . $space['name'] . "</td><td><a href='index.php?DLS=" . $space['space_id'] . "'>Delete</a></tr>";
+        }
+        echo "</table>";
+        $dbcon->close();
+    } else {
+        echo "no locations founded";
+    }
+}
+?>
+
 <h2>Create a space</h2>
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $dbcon = new mysqli($DBserver, $username, $password, $dbName);
+    if ($dbcon->connect_error) {
+        die("Connection error: " . $dbcon->connect_error);
+    }
+    $name_space = $_POST['name_space'];
+    $spaces_types = $_POST['spaces_types'];
+    $select_location = $_POST['Select_location'];
+    $insertQuery = "INSERT INTO spaces_tb(name,type,location_id) VALUES('$name_space','$spaces_types','$select_location')";
+    if ($dbcon->query($insertQuery) === true) {
+        echo "Space was registered";
+        echo "<meta http-equiv='refresh' content='1'>";
+    } else {
+        echo "Not submitted";
+    }
+    $dbcon->close();
+}
+?>
+
 <form method="POST">
     <input type="text" name="name_space" placeholder="Type the space name: ">
     <br>
@@ -9,43 +50,24 @@
     </select>
     <h3>Select the location: </h3>
     <select name="Select_location" required>
-     <?php
-         $dbcon = new mysqli($DBserver,$username,$password,$dbName);
-         if($dbcon->connect_error){
-             die("DB error");
-         }else{
-             $selectQuery = "SELECT * FROM locations_tb";
-             $LocationsList = $dbcon->query($selectQuery);
-             if($LocationsList->num_rows>0){
-                 while($Loc = $LocationsList->fetch_assoc()){
-                    echo "<option value='". $Loc['location_id']. "'>".$Loc['name']." ".$Loc['address']." ".$Loc['location_id']."</option>";
-                 }
-                 $dbcon->close();
-             }else{
-                 echo "no Locations founded";
-             }
-         }
-     ?>
+        <?php
+        $dbcon = new mysqli($DBserver, $username, $password, $dbName);
+        if ($dbcon->connect_error) {
+            die("DB error");
+        } else {
+            $selectQuery = "SELECT * FROM locations_tb";
+            $LocationsList = $dbcon->query($selectQuery);
+            if ($LocationsList->num_rows > 0) {
+                while ($Loc = $LocationsList->fetch_assoc()) {
+                    echo "<option value='" . $Loc['location_id'] . "'>" . $Loc['name'] . " " . $Loc['address'] . " " . $Loc['location_id'] . "</option>";
+                }
+                $dbcon->close();
+            } else {
+                echo "no Locations founded";
+            }
+        }
+        ?>
     </select>
     <br>
     <button type="submit">Register Space</button>
 </form>
- <?php
-        if($_SERVER['REQUEST_METHOD']=='POST'){
-            $dbcon = new mysqli($DBserver, $username, $password, $dbName);
-            if ($dbcon->connect_error) {
-              die("Connection error: " . $dbcon->connect_error);
-            }
-            $name_space=$_POST['name_space'];
-            $spaces_types=$_POST['spaces_types'];
-            $select_location=$_POST['Select_location'];
-            $insertQuery="INSERT INTO spaces_tb(name,type,location_id) VALUES('$name_space','$spaces_types','$select_location')";
-            if($dbcon->query($insertQuery)===true){
-                echo "Space was registered";
-            }
-            else{
-                echo "Not submitted";
-            }
-            $dbcon->close();
-        }
- ?>
